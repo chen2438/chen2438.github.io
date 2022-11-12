10d2f57bea2316204cf285e27f4c8a0b8559fd6a
chen2438 <2363822647@qq.com>
2022-11-12 18:52:25 +0800

    test

--- js/user.js
+++ js/user.js
@@ -1,15 +1,33 @@
+var requestURL = 'https://web-1303840613.cos.ap-shanghai.myqcloud.com/json/user.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
 
-
-window.onload = function () {
-    var url = "../json/user.json"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
-    var request = new XMLHttpRequest();
-    request.open("get", url);/*设置请求方法与路径*/
-    request.send(null);/*不发送数据到服务器*/
-    request.onload = function () {/*XHR对象获取到返回信息后执行*/
-        if (request.status == 200) {/*返回状态为200，即为数据获取成功*/
-            var json = JSON.parse(request.responseText);
-            console.log(json.name);
+request.onload = function () {
+    var userData = request.response;
+    var userID = getQueryID("id");
+    populateHeader(userData, userID);
+}
+function getQueryID(property) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] === property) {
+            return pair[1];
         }
-        console.log(json);
+    }
+    return -1;
+}
+function populateHeader(userData, userID) {
+    if (userID == 0) {
+        userName.textContent = '诸葛呼吸';
+    }
+    const userName = document.querySelector('.top-xm-1');
+    for (let i = 0; i < userData.length; i++) {
+        if (userData[i]["id"] == userID) {
+            userName.textContent = userData[i]["name"];
+        }
     }
 }
