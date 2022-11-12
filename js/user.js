d037e2f6105aead477499d5ee8438c205c80e2a1
chen2438 <2363822647@qq.com>
2022-11-12 15:16:35 +0800

    test

--- js/user.js
+++ js/user.js
@@ -1,3 +1,15 @@
-var user_name = document.querySelector('.top-xm-1');
-import data from '../json/user.json' assert { type: 'JSON' };
-console.log(data);
+
+
+window.onload = function () {
+    var url = "../json/user.json"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
+    var request = new XMLHttpRequest();
+    request.open("get", url);/*设置请求方法与路径*/
+    request.send(null);/*不发送数据到服务器*/
+    request.onload = function () {/*XHR对象获取到返回信息后执行*/
+        if (request.status == 200) {/*返回状态为200，即为数据获取成功*/
+            var json = JSON.parse(request.responseText);
+            console.log(json.name);
+        }
+        console.log(json);
+    }
+}
