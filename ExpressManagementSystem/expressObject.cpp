935ce3cd7057733969f4c69b7851091dc776f444
陈昊天 <2363822647@qq.com>
2022-12-14 17:38:31 +0800

    rm files

--- ExpressManagementSystem/expressObject.cpp
+++
@@ -1,6 +0,0 @@
-#include "expressObject.h";
-
-int User::login(string ID, string password, string userType) {
-    cout << ID << endl;
-    return 1;
-}
--- ExpressManagementSystem/head.h
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-#include <iostream>
-#include <string>
-
-using std::cin;
-using std::cout;
-using std::endl;
-using std::string;
